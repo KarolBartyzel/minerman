ec89faafa7005b80e9d2ea2cced4f279b5e16f82
Karol Bartyzel <karolbartyzel308@gmail.com>
2019-05-12 21:44:17 +0200

    Pause and result menu

---
+++ Assets/Scripts/Game/ResultMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResultMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject resultMenuUI;
+    public Text resultLabelUI;
+
+    void Start()
+    {
+        resultLabelUI.text = GlobalState.gameResult;
+    }
+
+    private void LoadResults()
+    {
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        resultLabelUI.text = GlobalState.gameResult;
+        resultMenuUI.SetActive(true);
+    }
+
+    public void GoToResults()
+    {
+        Invoke("LoadResults", 3f);
+    }
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ Assets/Scripts/Models/GameResult.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class GameResult
+{
+    public string playerName;
+    public int points;
+    public string status;
+}
